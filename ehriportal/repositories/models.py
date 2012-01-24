d961384e6cc769d99fb6bc924bea5f2e0aa45d9f
Mike Bryant <mikesname@gmail.com>
2012-01-24 13:40:14 +0000

    Added more fields to repository model and created a contact info model

--- ehriportal/repositories/models.py
+++ ehriportal/repositories/models.py
@@ -24,10 +24,49 @@ class Repository(models.Model):
     rules_conventions = models.TextField(null=True, blank=True)
     sources = models.TextField(null=True, blank=True)
     maintenance_notes = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(editable=False)
+    updated_at = models.DateTimeField(editable=False, null=True, blank=True)
 
+    def save(self):
+        if not self.id:
+            self.created_on = datetime.datetime.now()
+        else:
+            self.updated_on = datetime.datetime.now()
+        super(Repository, self).save()
 
     def __unicode__(self):
         return self.identifier
+
+
+class Contact(models.Model):
+    """Contact class."""
+    CONTACT_TYPES = ()
+
+    primary = models.BooleanField()
+    repository = models.ForeignKey(Repository, related_name="contacts")
+    contact_person = models.CharField(max_length=255, null=True, blank=True)
+    street_address = models.TextField(null=True, blank=True)
+    website = models.URLField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    telephone = models.CharField(max_length=100, null=True, blank=True)
+    fax = models.CharField(max_length=100, null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+    contact_type = models.CharField(
+            max_length=100, blank=True, null=True, choices=CONTACT_TYPES)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    region = models.CharField(max_length=100, null=True, blank=True)
+    note = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(editable=False)
+    updated_at = models.DateTimeField(editable=False, null=True, blank=True)
+
+    def save(self):
+        if not self.id:
+            self.created_on = datetime.datetime.now()
+        else:
+            self.updated_on = datetime.datetime.now()
+        super(Contact, self).save()
+
 
 class OtherName(models.Model):
     name = models.CharField(max_length=255)
