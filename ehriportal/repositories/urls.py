62d9fc3c4bf20c1f12cc160a31c75a0e8426211b
Mike Bryant <mikesname@gmail.com>
2012-01-26 13:54:44 +0000

    Added a search template and made the search view default for repositories

--- ehriportal/repositories/urls.py
+++ ehriportal/repositories/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import login_required
 from ehriportal.repositories import views
 
+from haystack.forms import FacetedSearchForm
+from haystack.query import SearchQuerySet
+
+sqs = SearchQuerySet().facet('country')
+
 urlpatterns = patterns('',
-   url(r'^/?$', views.RepoListView.as_view(), name="repos"),
+    url(r'^/?$', views.FacetedSearchView(
+        form_class=FacetedSearchForm, searchqueryset=sqs), name='repos'),
 )
--- ehriportal/urls.py
+++ ehriportal/urls.py
@@ -2,6 +2,10 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
+from haystack.forms import FacetedSearchForm
+from haystack.query import SearchQuerySet
+from haystack.views import FacetedSearchView
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,10 +15,15 @@ from pinax.apps.account.openid_consumer import PinaxConsumer
 handler500 = "pinax.views.server_error"
 
 
+sqs = SearchQuerySet().facet('country')
+
 urlpatterns = patterns("",
     url(r"^$", direct_to_template, {
         "template": "homepage.html",
     }, name="home"),
+    #url(r'^search/', include('haystack.urls')),
+    url(r'^search/', FacetedSearchView(form_class=FacetedSearchForm, searchqueryset=sqs), name='haystack_search'),
+
     url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user", name="admin_invite_user"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^about/", include("about.urls")),
