2374a0a55ed2c4e5d25c74df02fa3714843d2fec
Mike Bryant <mikesname@gmail.com>
2012-01-30 19:20:06 +0000

    Subclass the FacetedSearchView so we can sort the facet counts alphabetically instead of by count

--- ehriportal/portal/collection_urls.py
+++ ehriportal/portal/collection_urls.py
@@ -14,8 +14,18 @@ infodict = dict(
         queryset=models.Collection.objects.all()
 )
 
+class CollectionSearchView(FacetedSearchView):
+    def extra_context(self, *args, **kwargs):
+        extra = super(CollectionSearchView, self).extra_context(*args, **kwargs)
+        # sort counts, ideally we'd do this in the template
+        for facet in extra["facets"]["fields"].keys():
+            extra["facets"]["fields"][facet].sort(
+                    lambda x, y: cmp(x[0], y[0]))
+        return extra
+
+
 urlpatterns = patterns('',
-    url(r'^/?$', FacetedSearchView(
+    url(r'^/?$', CollectionSearchView(
         form_class=FacetedSearchForm, searchqueryset=sqs,
         template="portal/collection_search.html"), name='collection_search'),
     url(r'^(?P<slug>[-\w]+)/?$', object_detail, infodict, name='collection_detail'),
