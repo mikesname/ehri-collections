8de824d1f0782987e9cc075c556b5f8ef3dc4a3b
Mike Bryant <mikesname@gmail.com>
2012-02-05 21:57:24 +0000

    Move static files to where they belong (not in site_media)

---
+++ ehriportal/static/js/portal.js
@@ -0,0 +1,25 @@
+$(function($) {
+
+    $(".more-facets").each(function(i, elem) {
+        $(elem).modal({backdrop:true,
+            shown: function(e) { alert("show"); },
+        });
+    });
+
+    // add Ajax behaviour on pagination links
+    $(".ajax, .modal-body > .pagination ul li a").live("click", function(event) {
+        event.preventDefault();
+        // MASSIVE HACK - extract the facet class from the fclass data attribute
+        // that is written into the modal-body div and alter the various
+        // links to point the the particular facet list page.  This is deeply
+        // offensive to all that is good and pure...
+        if (this.href) {
+            var klass = $(".modal-body").data("fclass");
+            $("#modal-popup").load(this.href.replace(/search\?/, "search/" + klass + "/?"))
+        }
+    });
+    $(".facet-header").click(function(event) {
+        event.preventDefault();
+        $(this).parent().next("dl").toggle(200);
+    });
+});
