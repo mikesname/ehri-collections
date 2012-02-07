d663f5e5456b3907e6bd3fd2700ecaa301ce4403
Mike Bryant <mikesname@gmail.com>
2012-02-07 23:18:46 +0000

    Add a hacky method of subbing 'official' names which are awkward, for more handy ones. i.e. 'United Kingdom of Great Britain & Northern Ireland -> Great Britain.

--- ehriportal/portal/utils.py
+++ ehriportal/portal/utils.py
@@ -3,6 +3,13 @@
 from incf.countryutils import transformations
 import babel
 
+# Hacky dictionary of official country/languages names
+# we want to substitute for friendlier versions... 
+# A more permenant solution is needed to this.
+SUBNAMES = {
+    "United Kingdom of Great Britain & Northern Ireland": "United Kingdom",
+}
+
 def language_name_from_code(code, locale="en"):
     """Get lang display name."""
     # TODO: Find the correct way to do this
@@ -12,7 +19,8 @@ def language_name_from_code(code, locale="en"):
 def get_country_from_code(code):
     """Get the country code from a coutry name."""
     try:
-        return transformations.cc_to_cn(code)
+        name = transformations.cc_to_cn(code)
+        return SUBNAMES.get(name, name)
     except KeyError:
         pass
 
