5313432ba6529782bc91d527962c9d6265ebd64e
Mike Bryant <mikesname@gmail.com>
2012-02-07 15:30:41 +0000

    Make facet selector JS slightly less horrific and error prone.

--- ehriportal/static/js/portal.js
+++ ehriportal/static/js/portal.js
@@ -8,16 +8,17 @@ $(function($) {
 
     // add Ajax behaviour on pagination links
     $(".ajax, .modal-body > .pagination ul li a").live("click", function(event) {
-        event.preventDefault();
         // MASSIVE HACK - extract the facet class from the fclass data attribute
         // that is written into the modal-body div and alter the various
         // links to point the the particular facet list page.  This is deeply
         // offensive to all that is good and pure...
-        if (this.href) {
-            var klass = $(".modal-body").data("fclass");
-            $("#modal-popup").load(this.href.replace(/search\?/, "search/" + klass + "/?"))
+        var klass = $(".modal-body").data("fclass");
+        if (klass && this.href) {
+            event.preventDefault();
+            $("#modal-popup").load(this.href.replace(/search\?/, "search/" + klass + "/?"));
         }
     });
+
     $(".facet-header").click(function(event) {
         event.preventDefault();
         $(this).parent().next("dl").toggle(200);
