c21f10ef16d5fc5353316f370cdfe43bd51c3984
Mike Bryant <mikesname@gmail.com>
2012-02-07 13:26:03 +0000

    Clean up fabfile. Add various PIL dependencies. Flesh out a few more functions.

--- fabfile.py
+++ fabfile.py
@@ -8,9 +8,8 @@ env.project_name = 'ehriportal'
 
 @_contextmanager
 def virtualenv():
-    with cd(env.path):
-        with prefix(env.activate):
-            yield
+    with prefix(env.activate):
+        yield
 
 # environments
 def remote():
@@ -18,8 +17,8 @@ def remote():
     env.hosts = ['ehri01.dans.knaw.nl']
     env.path = '/home/michaelb/ehri-collections'
     env.user = 'michaelb'
-    env.activate = 'source ./bin/activate'
-    env.virtualhost_path = "/testportal"
+    env.activate = 'source %s/bin/activate' % env.path
+    env.solrconf = '/opt/apache-solr-3.5.0/portal/solr/conf'
 
 # tasks
 def test():
@@ -35,14 +34,18 @@ def setup():
     require('path')
     sudo('yum install -y python-setuptools')
     sudo('yum-builddep -y python26-mysqldb')
+    # PIL requirements
+    sudo('yum install -y libjpeg-devel')
+    sudo('yum install -y libpng-devel')
     sudo('easy_install pip')
     sudo('pip install virtualenv')
-    #sudo('yum install -y apache2')        
-    #sudo('yum install -y libapache2-mod-wsgi')
-    # we want rid of the defult apache config
-    #sudo('cd /etc/apache2/sites-available/; a2dissite default;')
-    run('mkdir -p %s; cd %s; virtualenv --python=python2.6 .;' % (env.path, env.path))
-    run('cd %s; mkdir -p releases; mkdir -p shared; mkdir -p packages;' % env.path)
+
+    run('mkdir -p %(path)s' % env)
+    with cd(env.path):
+        run('virtualenv --python=python2.6 .')
+        run('mkdir -p releases shared packages site_media/media site_media/static')
+        put("%(project_name)s/production_settings.py.sample" % env,
+                "shared/production_settings.py") 
     deploy()
 
 def deploy():
@@ -55,16 +58,19 @@ def deploy():
     env.release = time.strftime('%Y%m%d%H%M%S')
     upload_tar_from_git()
     install_requirements()
-    install_site()
     symlink_current_release()
+    activate_production_settings()
     migrate()
+    collectstatic()
     restart_webserver()
 
 def deploy_version(version):
     "Specify a specific version to be made live"
+    require('path')
     env.version = version
-    run('cd %s; rm releases/previous; mv releases/current releases/previous;' % env.path)
-    run('cd %s; ln -s %s releases/current' % (env.path, env.version))
+    with cd(env.path):
+        run('rm releases/previous; mv releases/current releases/previous;')
+        run('ln -s %(version)s releases/current' % env)
     restart_webserver()
 
 def rollback():
@@ -72,47 +78,88 @@ def rollback():
     Limited rollback capability. Simple loads the previously current
     version of the code. Rolling back again will swap between the two.
     """
-    run('cd %s; mv releases/current releases/_previous;' % env.path)
-    run('cd %s; mv releases/previous releases/current;' % env.path)
-    run('cd %s; mv releases/_previous releases/previous;' % env.path)
+    with cd(env.path):
+        run('mv releases/current releases/_previous')
+        run('mv releases/previous releases/current')
+        run('mv releases/_previous releases/previous')
     restart_webserver()    
 
 # Helpers. These are called by other functions rather than directly
 def upload_tar_from_git():
     "Create an archive from the current Git master branch and upload it"
-    local('git archive --format=tar master | gzip > %s.tar.gz' % env.release)
-    run('mkdir %s/releases/%s' % (env.path, env.release))
-    put('%s.tar.gz' % env.release, '%s/packages/' % env.path)
-    run('cd %s/releases/%s && tar zxf ../../packages/%s.tar.gz' % (env.path, env.release, env.release))
-    local('rm %s.tar.gz' % env.release)
-
-def install_site():
-    "Add the virtualhost file to apache"
-    #sudo('cd %s/releases/%s; cp %s%s%s/etc/apache2/sites-available/' % (
-    #    env.path, env.release, env.project_name, env.virtualhost_path, env.project_name))
-    #sudo('cd /etc/apache2/sites-available/; a2ensite %s' % env.project_name) 
+    require("path", "release")
+    local('git archive --format=tar master | gzip > %(release)s.tar.gz' % env)
+    run('mkdir %(path)s/releases/%(release)s' % env)
+    put('%(release)s.tar.gz' % env, '%(path)s/packages/' % env)
+    with cd("%(path)s/releases/%(release)s" % env):
+        run('tar zxf ../../packages/%(release)s.tar.gz' % env)
+    local('rm %(release)s.tar.gz' % env)
 
 def install_requirements():
     "Install the required packages from the requirements file using pip"
+    require("path", "project_name")
     with virtualenv():
-        run('cd %s; pip install -E . -r ./releases/%s/%s/requirements/project.txt' % (
-            env.path, env.release, env.project_name))
+        with cd(env.path):
+            run('pip install -E . -r ./releases/%s/%s/requirements/project.txt' % (
+                env.release, env.project_name))
 
 def symlink_current_release():
     "Symlink our current release"
+    require("path", "release")
     with settings(warn_only=True):
-        run('cd %s; rm -f releases/previous; mv releases/current releases/previous;' % env.path)
-    run('cd %s; ln -s %s releases/current' % (env.path, env.release))
+        with cd(env.path):
+            run('rm -f releases/previous; mv releases/current releases/previous;')
+            run('ln -s %(release)s releases/current' % env)
+
+def activate_production_settings():
+    """Copy shared/production_settings.py to the release
+    folder where they will be imported by settings.py"""
+    require("path", "release")
+    with cd(env.path):
+        run("cp shared/production_settings.py releases/%(release)s/%(project_name)s/production_settings.py" % env)
+
 
 def migrate():
     "Update the database"
     with virtualenv():
-        run('cd %s/releases/current/%s; python manage.py syncdb --noinput' % (
-            env.path, env.project_name))
-        run('cd %s/releases/current/%s; python manage.py migrate' % (
-            env.path, env.project_name))
-        run('cd %s/releases/current/%s; mkdir -p media; python manage.py collectstatic --noinput' % (
-            env.path, env.project_name))
+        with cd("%(path)s/releases/%(release)s/%(project_name)s" % env):
+            run('python manage.py syncdb --noinput')
+            run('python manage.py migrate')
+
+def collectstatic():
+    "Save static files to serve location."
+    with virtualenv():
+        with cd("%(path)s/releases/%(release)s/%(project_name)s" % env):
+            run('python manage.py collectstatic --noinput')
+
+def update_solr_schema():
+    """Update the Solr schema.xml."""
+    require("path", "project_name", "solrconf")
+    with virtualenv():
+        with cd("%(path)s/releases/current/%(project_name)s" % env):
+            run('python manage.py build_solr_schema > _schema.xml')
+            sudo('mv _schema.xml %(solrconf)s/schema.xml' % env)
+            restart_solr()
+            # fudge
+            run('sleep 10')
+            rebuild_index()
+
+def update_index():
+    """Rebuild document index from database."""
+    with virtualenv():
+        with cd("%(path)s/releases/current/%(project_name)s" % env):
+            run('python manage.py update_index')
+
+def rebuild_index():
+    """Rebuild document index from database."""
+    with virtualenv():
+        with cd("%(path)s/releases/current/%(project_name)s" % env):
+            run('python manage.py rebuild_index --noinput')
+
+def restart_solr():
+    "Restart Solr (via Jetty)"
+    sudo('/etc/init.d/jetty restart')
+
 
 def restart_webserver():
     "Restart the web server"
