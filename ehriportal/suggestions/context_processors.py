e927bf15555e29cd76d970eb5f8b98dbdf1e8a76
Mike Bryant <mikesname@gmail.com>
2012-02-08 21:44:52 +0000

    Added a context_processor for the suggestions app so you can add the form object to every page.

---
+++ ehriportal/suggestions/context_processors.py
@@ -0,0 +1,14 @@
+"""Add the form to every request's context, pre-populated by
+some useful information."""
+
+from suggestions import forms
+
+def suggestions_form(request):
+    initial = {}
+    if request.user.is_authenticated():
+        initial["name"] = request.user.get_full_name() \
+                or request.user.username
+        initial["email"]  = request.user.email
+    form = forms.SuggestionForm(initial=initial, prefix="suggestion")
+    return dict(suggestionform=form)
+
