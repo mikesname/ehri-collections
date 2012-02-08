1cdd8add2807ecedb7efb23428075423dcf9bfd2
Mike Bryant <mikesname@gmail.com>
2012-02-08 22:41:18 +0000

    Add some translation stuff on the suggestion form

--- ehriportal/suggestions/forms.py
+++ ehriportal/suggestions/forms.py
@@ -1,19 +1,20 @@
 """Form for submitting suggestions."""
 
 from django import forms
+from django.utils.translation import ugettext as _
 
 from suggestions import models
 
 class SuggestionForm(forms.ModelForm):
-    name = forms.CharField(max_length=100, label="Name",
-            widget=forms.TextInput(attrs={'placeholder': 'Name'}))
-    email = forms.EmailField(label="Email", required=False,
-            widget=forms.TextInput(attrs={'placeholder': 'Email (Optional)'}))
+    name = forms.CharField(max_length=100, label=_("Name"),
+            widget=forms.TextInput(attrs={'placeholder': _('Name')}))
+    email = forms.EmailField(label=_("Email"), required=False,
+            widget=forms.TextInput(attrs={'placeholder': _('Email (Optional)')}))
     types = forms.ModelMultipleChoiceField(
                 required=False,
-                queryset=models.SuggestionType.objects.all().order_by("name"))
+                queryset=models.SuggestionType.objects.all().order_by(_("name")))
     text = forms.CharField(widget=forms.Textarea(
-                attrs={'rows':5, 'placeholder': "Comments"}))
+                attrs={'rows':5, 'placeholder': _("Let us know what you think...")}))
 
     class Meta:
         model = models.Suggestion
