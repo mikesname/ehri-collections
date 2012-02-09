98e58a9e7994d32d604e97d042e3ad72c29c56dd
Mike Bryant <mikesname@gmail.com>
2012-02-09 22:33:59 +0000

    Use Sqlite3 for testing

--- ehriportal/devel_settings.py
+++ ehriportal/devel_settings.py
@@ -1,6 +1,18 @@
 
+import sys
+
 ADMINS = (
         ("Mike", "michael.bryant@kcl.ac.uk"),
 )
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": "test.db",                   
+        }
+    }
+
+
