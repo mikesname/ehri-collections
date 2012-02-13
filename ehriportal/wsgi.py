9dfd327dfad8f7a776721eb46e52893b526e58d8
Mike Bryant <mikesname@gmail.com>
2012-02-13 15:26:04 +0000

    Attempt to make wsgi script more virtualenv-friendly

--- ehriportal/wsgi.py
+++ ehriportal/wsgi.py
@@ -3,7 +3,10 @@ import sys
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, PROJECT_ROOT)
-sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "../../../lib/python2.6/site-packages")))
+
+venv_path = os.path.abspath(os.path.join(PROJECT_ROOT, "../../../"))
+activate_this = os.path.join(venv_path, "bin/activate_this.py")
+execfile(activate_this, dict(__file__=activate_this))
 
 
 from django.core.handlers.wsgi import WSGIHandler
