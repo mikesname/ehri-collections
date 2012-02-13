ae7690b59ae09613d20150333c85427df97b556f
Mike Bryant <mikesname@gmail.com>
2012-02-13 15:42:22 +0000

    Ensure DJANGO_SETTINGS_MODULE is ALWAYS set, otherwise it's overwritten by other apps on the system

--- ehriportal/wsgi.py
+++ ehriportal/wsgi.py
@@ -2,6 +2,8 @@ import os
 import sys
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_NAME = os.path.basename(PROJECT_ROOT)
+
 sys.path.insert(0, PROJECT_ROOT)
 
 venv_path = os.path.abspath(os.path.join(PROJECT_ROOT, "../../../"))
@@ -9,7 +11,7 @@ activate_this = os.path.join(venv_path, "bin/activate_this.py")
 execfile(activate_this, dict(__file__=activate_this))
 
 
-sys.stderr.write("WSGI Python Path: %s\n" % sys.path)
+sys.stderr.write("WSGI Python Path (Collections): %s\n" % sys.path)
 
 from django.core.handlers.wsgi import WSGIHandler
 
@@ -18,6 +20,7 @@ import pinax.env
 # setup the environment for Django and Pinax
 pinax.env.setup_environ(__file__)
 
+os.environ['DJANGO_SETTINGS_MODULE'] = '%s.settings' % PROJECT_NAME
 
 # set application for WSGI processing
 application = WSGIHandler()
