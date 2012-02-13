145a0c0b4b16d61ae839d4e386a446d187b8ad63
Mike Bryant <mikesname@gmail.com>
2012-02-13 13:58:52 +0000

    Test.  Add pythonpath in wsgi script

--- ehriportal/wsgi.py
+++ ehriportal/wsgi.py
@@ -1,7 +1,9 @@
+import sys
+sys.path.insert(0, "/home/michaelb/ehri-collections/lib/python2.6/site-packages")
+
 from django.core.handlers.wsgi import WSGIHandler
 
 import pinax.env
-
 
 # setup the environment for Django and Pinax
 pinax.env.setup_environ(__file__)
