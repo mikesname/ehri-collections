ec38c0ed65bc534dcf1eab9572ac56a3274b434e
Mike Bryant <mikesname@gmail.com>
2012-03-05 13:56:55 +0000

    Don't use sqlite yet, since it's harder to get to work with GIS

--- ehriportal/devel_settings.py
+++ ehriportal/devel_settings.py
@@ -7,12 +7,4 @@ ADMINS = (
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-if "test" in sys.argv:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": "test.db",                   
-        }
-    }
-
 YAHOO_APP_ID = "33_udMTV34GnWuHaUYyuZXH.wkBvwNDroY74jndgzW0lNtePUNMb7GBk91hYdxAc"
