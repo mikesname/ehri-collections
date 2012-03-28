fbaf787b39c23df67343f3de786f3452373c4412
Mike Bryant <mikesname@gmail.com>
2012-03-28 10:02:01 +0100

    Tweak devel settings to use spatialite for testing

--- ehriportal/devel_settings.py
+++ ehriportal/devel_settings.py
@@ -8,3 +8,12 @@ ADMINS = (
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 YAHOO_APP_ID = "33_udMTV34GnWuHaUYyuZXH.wkBvwNDroY74jndgzW0lNtePUNMb7GBk91hYdxAc"
+
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.contrib.gis.db.backends.spatialite",
+            "NAME": "test.sqlite",                       # Or path to database file if using sqlite3.
+        }
+    }
+    SPATIALITE_SQL = "conf/init_spatialite-2.3.sql"
