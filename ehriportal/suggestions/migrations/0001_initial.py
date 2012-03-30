34abebf72aaa6fba629b9b8e41ca94b6926d217d
Mike Bryant <mikesname@gmail.com>
2012-03-30 12:43:37 +0100

    Reset south migrations for suggestions app

--- ehriportal/suggestions/migrations/0001_initial.py
+++ ehriportal/suggestions/migrations/0001_initial.py
@@ -8,43 +8,23 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         
-        # Adding model 'SuggestionType'
-        db.create_table('suggestions_suggestiontype', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-        ))
-        db.send_create_signal('suggestions', ['SuggestionType'])
-
         # Adding model 'Suggestion'
         db.create_table('suggestions_suggestion', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, null=True, blank=True)),
             ('text', self.gf('django.db.models.fields.TextField')()),
+            ('meta', self.gf('jsonfield.fields.JSONField')(null=True)),
             ('created_on', self.gf('django.db.models.fields.DateTimeField')()),
             ('updated_on', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
         ))
         db.send_create_signal('suggestions', ['Suggestion'])
 
-        # Adding M2M table for field types on 'Suggestion'
-        db.create_table('suggestions_suggestion_types', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('suggestion', models.ForeignKey(orm['suggestions.suggestion'], null=False)),
-            ('suggestiontype', models.ForeignKey(orm['suggestions.suggestiontype'], null=False))
-        ))
-        db.create_unique('suggestions_suggestion_types', ['suggestion_id', 'suggestiontype_id'])
-
 
     def backwards(self, orm):
         
-        # Deleting model 'SuggestionType'
-        db.delete_table('suggestions_suggestiontype')
-
         # Deleting model 'Suggestion'
         db.delete_table('suggestions_suggestion')
-
-        # Removing M2M table for field types on 'Suggestion'
-        db.delete_table('suggestions_suggestion_types')
 
 
     models = {
@@ -53,15 +33,10 @@ class Migration(SchemaMigration):
             'created_on': ('django.db.models.fields.DateTimeField', [], {}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'meta': ('jsonfield.fields.JSONField', [], {'null': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'text': ('django.db.models.fields.TextField', [], {}),
-            'types': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['suggestions.SuggestionType']", 'symmetrical': 'False'}),
             'updated_on': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'})
-        },
-        'suggestions.suggestiontype': {
-            'Meta': {'object_name': 'SuggestionType'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
         }
     }
 
