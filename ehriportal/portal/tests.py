4ef1654618328bf252bcbaf107e45101dbb890d1
Mike Bryant <mikesname@gmail.com>
2012-04-17 10:59:59 +0100

    Refactored out some duplication in Collection tests, which now have their own class.

--- ehriportal/portal/tests.py
+++ ehriportal/portal/tests.py
@@ -10,7 +10,7 @@ from django.core.urlresolvers import reverse
 
 from portal import models
 
-class PortalTest(TestCase):
+class PortalRepositoryTest(TestCase):
     fixtures = ["resource.json", "repository.json", "collection.json"]
     def setUp(self):
         pass
@@ -21,11 +21,6 @@ class PortalTest(TestCase):
     def test_repo_list(self):
         """Test list of repositories."""
         response = self.client.get(reverse("repo_list"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_collection_list(self):
-        """Test list of collections."""
-        response = self.client.get(reverse("collection_list"))
         self.assertEqual(response.status_code, 200)
 
     def test_repo_collections(self):
@@ -48,11 +43,34 @@ class PortalTest(TestCase):
             "slug": "wiener-library",
         }))
         self.assertEqual(response.status_code, 200)
-        
+
+
+class PortalCollectionTest(TestCase):
+    fixtures = ["resource.json", "repository.json", "collection.json"]
+    def setUp(self):
+        self.slug = "caro-jella-letter-from-theresienstadt"
+        self.testdata = {
+
+        }
+        for field in ["date_set", "othername_set",
+                "script", "script_of_description",
+                "language", "language_of_description"]:
+            self.testdata["%s-INITIAL_FORMS" % field] = 0 
+            self.testdata["%s-MAX_NUM_FORMS" % field] = 1
+            self.testdata["%s-TOTAL_FORMS" % field] = 1
+
+    def tearDown(self):
+        pass
+
+    def test_collection_list(self):
+        """Test list of collections."""
+        response = self.client.get(reverse("collection_list"))
+        self.assertEqual(response.status_code, 200)
+
     def test_collection_detail(self):
         """Test collection detail view."""
         response = self.client.get(reverse("collection_detail", kwargs={
-            "slug": "caro-jella-letter-from-theresienstadt",
+            "slug": self.slug,
         }))
         self.assertEqual(response.status_code, 200)
 
@@ -67,14 +85,13 @@ class PortalTest(TestCase):
         testname = "Test Create"
         self.assertEqual(models.Collection.objects.filter(
                 name=testname).count(), 0)
-        params = {
+        self.testdata.update({
             "identifier": "GB Test 0001",
             "name": testname,
             "repository": repo.pk,
             "language-0-value": "en",
-        }
-        params.update(self._get_collection_formset_metadata())
-        response = self.client.post(reverse("collection_create"), params)
+        })
+        response = self.client.post(reverse("collection_create"), self.testdata)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(models.Collection.objects.filter(
                 name=testname).count(), 1)
@@ -85,72 +102,73 @@ class PortalTest(TestCase):
     def test_collection_edit_get(self):
         """Test collection edit view."""
         response = self.client.get(reverse("collection_edit", kwargs={
-            "slug": "caro-jella-letter-from-theresienstadt",
+            "slug": self.slug,
         }))
         self.assertEqual(response.status_code, 200)
 
     def test_collection_edit_post(self):
         """Test updating a collection."""
-        slug = "caro-jella-letter-from-theresienstadt"
-        c = models.Collection.objects.get(slug=slug)
-        params = {
+        c = models.Collection.objects.get(slug=self.slug)
+        self.testdata.update({
             "identifier": "Test",
             "name": "Test",
             "repository": c.repository.pk
-        }
-        params.update(self._get_collection_formset_metadata())
+        })
         response = self.client.post(reverse("collection_edit", kwargs={
-            "slug": slug,
-        }), params)
+            "slug": self.slug,
+        }), self.testdata)
         self.assertEqual(response.status_code, 302)
-        c = models.Collection.objects.get(slug=slug)
+        c = models.Collection.objects.get(slug=self.slug)
         self.assertEqual(c.name, "Test")
         
     def test_collection_edit_add_langprop(self):
         """Test updating a collection."""
-        slug = "caro-jella-letter-from-theresienstadt"
-        c = models.Collection.objects.get(slug=slug)
+        c = models.Collection.objects.get(slug=self.slug)
         self.assertNotIn("de", c.languages)
-        params = {
+        self.testdata.update({
             "identifier": "Test",
             "name": "Test",
             "repository": c.repository.pk,
             "language-0-value": "de",
-        }
-        params.update(self._get_collection_formset_metadata())
+        })
         response = self.client.post(reverse("collection_edit", kwargs={
-            "slug": slug,
-        }), params)
+            "slug": self.slug,
+        }), self.testdata)
         self.assertEqual(response.status_code, 302)
-        c = models.Collection.objects.get(slug=slug)
+        c = models.Collection.objects.get(slug=self.slug)
         self.assertIn("de", c.languages)
         
     def test_collection_edit_add_alt_name(self):
         """Test updating a collection."""
-        slug = "caro-jella-letter-from-theresienstadt"
-        c = models.Collection.objects.get(slug=slug)
+        c = models.Collection.objects.get(slug=self.slug)
         self.assertNotIn("Alt Test", c.other_names)
-        params = {
+        self.testdata.update({
             "identifier": "Test",
             "name": "Test",
             "repository": c.repository.pk,
             "othername_set-0-name": "Alt Test",
-        }
-        params.update(self._get_collection_formset_metadata())
+        })
         response = self.client.post(reverse("collection_edit", kwargs={
-            "slug": slug,
-        }), params)
+            "slug": self.slug,
+        }), self.testdata)
         self.assertEqual(response.status_code, 302)
-        c = models.Collection.objects.get(slug=slug)
+        c = models.Collection.objects.get(slug=self.slug)
         self.assertIn("Alt Test", c.other_names)
         
-    def _get_collection_formset_metadata(self):
-        meta = {}
-        for field in ["date_set", "othername_set",
-                "script", "script_of_description",
-                "language", "language_of_description"]:
-            meta["%s-INITIAL_FORMS" % field] = 0 
-            meta["%s-MAX_NUM_FORMS" % field] = 1
-            meta["%s-TOTAL_FORMS" % field] = 1
-        return meta
-
+    def test_collection_edit_with_error(self):
+        """Test updating a collection."""
+        c = models.Collection.objects.get(slug=self.slug)
+        self.assertNotIn("Alt Test", c.other_names)
+        self.testdata.update({
+            "identifier": "",
+            "name": "Test",
+            "repository": c.repository.pk,
+            "othername_set-0-name": "Alt Test",
+        })
+        response = self.client.post(reverse("collection_edit", kwargs={
+            "slug": self.slug,
+        }), self.testdata)
+        self.assertEqual(response.status_code, 200)
+        c = models.Collection.objects.get(slug=self.slug)
+        self.assertNotIn("Alt Test", c.other_names)
+        
