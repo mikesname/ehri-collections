ae3e5c007a7a7811abd46aef463ea7544f195b32
Mike Bryant <mikesname@gmail.com>
2012-04-19 17:06:32 +0100

    Beginnings of a Crud mixin class for all the woefully copy-pasted entity Crud tests

--- ehriportal/portal/tests.py
+++ ehriportal/portal/tests.py
@@ -10,6 +10,130 @@ from django.contrib.auth.models import User
 
 from portal import models
 
+
+class EntityCrudTestCase(object):
+    """Mixin class which defines a lot of boilerplate
+    CRUD-deleted tests."""
+    def test_list(self):
+        """Test list of objects."""
+        response = self.client.get(reverse(self.urlprefix + "_list"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail(self):
+        """Test object detail view."""
+        response = self.client.get(reverse(self.urlprefix + "_detail", kwargs={
+            "slug": self.slug,
+        }))
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_without_credentials(self):
+        """Try accessing the edit page without being a member of staff."""
+        self.client.logout()
+        user = User.objects.create_user("random", password="password")
+        user.is_staff = False
+        user.save()
+        self.client.login(username=user.username, password="password")
+        response = self.client.get(reverse(self.urlprefix + "_edit", kwargs={
+            "slug": self.slug,
+        }))
+        self.assertEqual(response.status_code, 302)
+
+    def test_create_get(self):
+        """Test object create view."""
+        response = self.client.get(reverse(self.urlprefix + "_create"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_post(self):
+        """Test creating a object."""
+        repo = models.Repository.objects.all()[0]
+        testname = "Test Create"
+        self.assertEqual(self.model.objects.filter(
+                name=testname).count(), 0)
+        self.testdata.update(self.updatedata)
+        response = self.client.post(reverse(self.urlprefix + "_create"), self.testdata)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.model.objects.filter(
+                name=testname).count(), 1)
+        c = self.model.objects.filter(name=testname)[0]
+        self.assertIn("en", c.languages)
+        self.assertEqual(testname, c.name)
+        
+    def test_edit_get(self):
+        """Test  edit view."""
+        response = self.client.get(reverse(self.urlprefix + "_edit", kwargs={
+            "slug": self.slug,
+        }))
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_post(self):
+        """Test updating a object."""
+        c = self.model.objects.get(slug=self.slug)
+        self.testdata.update(self.updatedata)
+        response = self.client.post(reverse(self.urlprefix + "_edit", kwargs={
+            "slug": self.slug,
+        }), self.testdata)
+        self.assertEqual(response.status_code, 302)
+        c = self.model.objects.get(slug=self.slug)
+        self.assertEqual(c.name, "Test")
+        
+    def test_edit_add_langprop(self):
+        """Test updating a object."""
+        c = self.model.objects.get(slug=self.slug)
+        self.assertNotIn("de", c.languages)
+        self.testdata.update(self.updatedata)
+        self.testdata.update({
+            "language-0-value": "de",
+        })
+        response = self.client.post(reverse(self.urlprefix + "_edit", kwargs={
+            "slug": self.slug,
+        }), self.testdata)
+        self.assertEqual(response.status_code, 302)
+        c = self.model.objects.get(slug=self.slug)
+        self.assertIn("de", c.languages)
+        
+    def test_edit_add_alt_name(self):
+        """Test updating a object."""
+        c = self.model.objects.get(slug=self.slug)
+        self.assertNotIn("Alt Test", c.other_names)
+        self.testdata.update(self.updatedata)
+        response = self.client.post(reverse(self.urlprefix + "_edit", kwargs={
+            "slug": self.slug,
+        }), self.testdata)
+        self.assertEqual(response.status_code, 302)
+        c = self.model.objects.get(slug=self.slug)
+        self.assertIn("Alt Test", c.other_names)
+        
+    def test_edit_with_error(self):
+        """Test updating a object."""
+        c = self.model.objects.get(slug=self.slug)
+        self.assertNotIn("Alt Test", c.other_names)
+        self.testdata.update(self.updatedata)
+        response = self.client.post(reverse(self.urlprefix + "_edit", kwargs={
+            "slug": self.slug,
+        }), self.testdata)
+        self.assertEqual(response.status_code, 200)
+        c = self.model.objects.get(slug=self.slug)
+        self.assertNotIn("Alt Test", c.other_names)
+
+    def test_delete_confirm(self):
+        """Test deleting a object - first step."""
+        response = self.client.get(reverse(self.urlprefix + "_delete", kwargs={
+            "slug": self.slug,
+        }))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_delete(self):
+        """Test deleting a object - second step."""
+        ccount = self.model.objects.count()
+        response = self.client.post(reverse(self.urlprefix + "_delete", kwargs={
+            "slug": self.slug,
+        }))
+        self.assertEqual(response.status_code, 302)
+        ccount2 = self.model.objects.count()
+        self.assertEqual(ccount, ccount2 + 1)
+        
+
+
 class PortalRepositoryTest(TestCase):
     fixtures = ["resource.json", "repository.json", "collection.json"]
     def setUp(self):
