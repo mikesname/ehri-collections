57b484d1e867b6d541c2c071cdf065cc32ee38e4
Mike Bryant <mikesname@gmail.com>
2012-05-01 14:01:58 +0100

    Convert auth/repo forms to new inline formset style, and make sure we leave the delete buttons for the contact forms visible

--- ehriportal/static/js/dynamic-formsets.js
+++ ehriportal/static/js/dynamic-formsets.js
@@ -12,7 +12,8 @@
 jQuery(document).ready(function($) {
 
     // hide the delete checkboxes
-    $("input[id$='-DELETE']").add("label[for$='-DELETE']").hide();
+    $("input[id$='-DELETE']", ".inlinemulti")
+        .add("label[for$='-DELETE']", ".inlinemulti").hide();
 
     // remove existing inline elements when clicked
     $(document).on("click", ".inline-proxy", function(event) {
@@ -73,82 +74,8 @@ jQuery(document).ready(function($) {
         var prefix = $(this).data("prefix"),
             initialforms = parseInt($("#id_" + prefix + "-INITIAL_FORMS").val());
         for (var i = 0; i < initialforms; i++) {
-            console.log("setreadonly", prefix, i);
             setReadOnly(prefix, i);
         }
     });
 })
-
-//jQuery(document).ready(function($) {
-//
-//    // remove existing inline elements when clicked
-//    $(document).on("click", ".inlineitem", function(event) {
-//        $(this).find("input[type='checkbox']")
-//            .prop("checked", true)
-//            .end()
-//            .hide(200);
-//    });
-//
-//    // add new inline elements when enter or tab pressed in their input
-//    $(document).on("keydown", ".inlinemulti > input:visible", function(event) {
-//        if ($.trim($(this).val()) != "" && (event.keyCode == 9 || event.keyCode == 13)) {
-//            if (this.id.match(/id_([^\d]+)-(\d+)-[^\d]+/)) {
-//                event.preventDefault();
-//                var prefix = RegExp.$1, index = parseInt(RegExp.$2);
-//                addNewAfter(index, prefix);
-//                setReadOnly(index, prefix);
-//            }
-//        }
-//    });
-//
-//    // hide the delete checkboxes
-//    $("input[id$='-DELETE']").add("label[for$='-DELETE']").hide();
-//
-//    // add a placeholder to the visible fields.
-//    $(".inlinemulti > input[type='text]:visible").each(function(i, elem) {
-//        $(elem).attr("placeholder", "Add New " + $(elem).closest("label").text());
-//    });
-//
-//    function setReadOnly(index, prefix) {
-//        var div = $("#id_form-" + prefix + "-" + index),
-//            idfield = $("#id_" + prefix + "-" + index + "-id"),
-//            inputs = idfield.nextUntil("input[type='checkbox']"),
-//            check = inputs.next("input[type='checkbox']"),
-//            valstr = inputs.map(function(i, e) { return $(e).val();}).toArray().join(" - ");
-//        inputs.hide().last().after($("<ul><li>" + valstr + "</li></ul>"));
-//        div.addClass("inlineitem");
-//    }
-//
-//    function addNewAfter(index, prefix) {
-//        var totalforms = parseInt($("#id_" + prefix + "-TOTAL_FORMS").val()),
-//            initialforms = parseInt($("#id_" + prefix + "-INITIAL_FORMS").val()),
-//            div = $("#id_form-" + prefix + "-" + index),
-//            newdiv = div.clone(),
-//            newindex = index + 1;
-//        newdiv
-//            .attr("id", "id_form-" + prefix + "-" + newindex)
-//            .data("prefix", prefix + "-" + newindex)
-//            .find("input").val(null);
-//        newdiv.find("label, input").each(function(i, elem) {
-//            $.each(["id", "name", "for"], function(i, attr) {
-//                if ($(elem).attr(attr)) {
-//                    $(elem).attr(attr, $(elem).attr(attr).replace(
-//                            prefix + "-" + index, prefix + "-" + newindex));
-//                }
-//            });
-//        });
-//        var name = $("#id_formset-" + prefix).find("label").first().text();
-//        newdiv.find("inputs[type='text']").attr("placeholder", "Add New " + name);
-//        $("#id_" + prefix + "-TOTAL_FORMS").val(totalforms + 1);
-//        div.after(newdiv).find("input[type='text']").first().focus();
-//    }
-//            
-//    $(".dynamic-formset").each(function(i, elem) {
-//        var prefix = $(this).data("prefix"),
-//            initialforms = parseInt($("#id_" + prefix + "-INITIAL_FORMS").val());
-//        for (var i = 0; i < initialforms; i++) {
-//            setReadOnly(i, prefix);
-//        }
-//    });
-//});
 
