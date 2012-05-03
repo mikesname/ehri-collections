b97c81f5fbb7997b6b3e9293992e793db48e7b1f
Mike Bryant <mikesname@gmail.com>
2012-05-03 13:43:40 +0100

    Add an override loaddata command to disconnect Haystack's realtime signals.

---
+++ ehriportal/portal/management/commands/loaddata.py
@@ -0,0 +1,15 @@
+"""
+Override loaddata so it disconnects Haystack signals before running.
+"""
+import sys
+from django.db.models import signals
+from django.core.management.commands.loaddata import Command as LoadDataCommand
+
+class Command(LoadDataCommand):
+    def handle(self, *args, **kwargs):
+        # disconnect Haystack realtime signals
+        signals.pre_save.disconnect(dispatch_uid="setup_index_signals")
+        signals.pre_delete.disconnect(dispatch_uid="setup_index_signals")
+        super(Command, self).handle(*args, **kwargs)
+        self.stdout.write("Haystack index not updated.\n")
+
