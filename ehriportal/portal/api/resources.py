46a83ce67daa5d5c495b8471ab1afdca22642659
Mike Bryant <mikesname@gmail.com>
2012-05-08 13:03:38 +0100

    Started some tentative initial work on EAD import/export (which isn't going to be ready for some time.)

--- ehriportal/portal/api/resources.py
+++ ehriportal/portal/api/resources.py
@@ -3,8 +3,33 @@ Tastypie resources for notable models.
 """
 
 
-from tastypie import resources
+from tastypie import resources, serializers
 from portal import models
+
+# Unused/unfinished collection exporter/importer that
+# supports EAD.
+class CollectionSerializer(serializers.Serializer):
+    formats = ['json', 'jsonp', 'xml', 'yaml', 'html', 'plist', 'ead']
+    content_types = {
+        'json': 'application/json',
+        'jsonp': 'text/javascript',
+        'xml': 'application/xml',
+        'yaml': 'text/yaml',
+        'html': 'text/html',
+        'plist': 'application/x-plist',
+        'ead': 'application/xml',
+    }
+
+    # NOTE: This is an incredibly naive implementation
+    # meant only as a proof of concept
+    def to_ead(self, data, options):
+        options = options or {}
+        data = self.to_simple(data, options)
+
+    def from_ead(self, data, options):
+        pass
+
+
 
 
 # Yes, the name is all sorts of wrong
